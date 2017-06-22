a2bba11a8702936919fa88bc0f0172e99c6a872f
bluelabuser <bluelabuser@pc01.bluelab.uct.ac.za>
2017-06-22 22:11:07 +0200

    row and column size fixes in imm

--- YODA_IMA2/imm_tb.v
+++ YODA_IMA2/imm_tb.v
@@ -6,16 +6,16 @@ module imm_tb;
 	reg clk;
 	reg [11:0] image_pixel;
 	//reg [11:0] mask_pixel;
-	reg [8:0] pixel_row;
-	reg [7:0] pixel_col;
-	reg [8:0] mask_row_offset;
-	reg [7:0] mask_col_offset;
+	reg [7:0] pixel_row;
+	reg [8:0] pixel_col;
+	reg [7:0] mask_row_offset;
+	reg [8:0] mask_col_offset;
 
 	// Outputs
 	//wire [6:0] mask_row;
 	//wire [7:0] mask_col;
-	wire [8:0] pixel_row_out;
-	wire [7:0] pixel_col_out;
+	wire [7:0] pixel_row_out;
+	wire [8:0] pixel_col_out;
 	wire [11:0] pixel_result;
 
 	// Instantiate the Unit Under Test (UUT)
@@ -49,8 +49,8 @@ module imm_tb;
 		#100	clk = 1;
 		//#50	mask_pixel = 12'h_AAA;
 		#100	clk = 0;
-		pixel_col = 7'b_0101_1000;
-		pixel_row = 8'b_001_0101;
+		pixel_col = 0;
+		pixel_row = 0;
 		image_pixel = 12'h_CBD;
 		
 		#100 clk = 1;
