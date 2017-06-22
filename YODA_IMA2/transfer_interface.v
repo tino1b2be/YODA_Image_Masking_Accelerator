28b6cb91792648a42bd26de81e7cf4b68a6b324f
bluelabuser <bluelabuser@pc01.bluelab.uct.ac.za>
2017-06-22 19:08:13 +0200

    transfer interface complete

--- YODA_IMA2/transfer_interface.v
+++ YODA_IMA2/transfer_interface.v
@@ -1,46 +1,45 @@
-module transfer_interface(Clock, pixel_in, pixel_out, pix_row, pix_col, strobe);
-input Clock;
-input [11:0] pixel_in;
+`include "utils.v"
 
-output [11:0] pixel_out;
-output [7:0] pix_row;
-output [8:0] pix_col;
-output strobe;
+module transfer_interface(
+input Clock,
 
+output reg [11:0] pixel_out,
+output reg [7:0] pix_row,
+output reg [8:0] pix_col
+);
 
-reg strobe;
-reg mode = 0; // mode = 0: row_sel and col_sel set for BRAM		mode = 1: infor written to IMM
-reg [11:0] pixel_out;
-reg [8:0] row_count = 0;
-reg [7:0] col_count = 0;
-reg [7:0] pix_row;
-reg [8:0] pix_col;
+reg strobe_reg;
+wire [11:0] pixel_in_reg;
+reg [11:0] pixel_out_reg;
+reg [7:0] row_count = 0;
+reg [8:0] col_count = 0;
+reg [7:0] pix_row_reg;
+reg [8:0] pix_col_reg;
 
 	// instantiate the BROM that stores the image.
 	 
 	image_rom image(
 		.row(row_count),					// image row address line
 		.col(col_count),					// image col address line
-		.color_data(pixel_in)	// mask pixel output bus
+		.color_data(pixel_in_reg)	// mask pixel output bus
 	);
-
+	
+	always @(posedge Clock) begin
+		col_count = col_count + 1;
+	end
+	
 	always @(*) begin
-		pixel_out = pixel_in;
+		pixel_out = pixel_in_reg;
 		pix_row = row_count;
 		pix_col = col_count;
-		
-		col_count = col_count + 1;
-		if(col_count > 6) // TODO Replace with actual column width
+		if(col_count > `IMAGE_WIDTH) // TODO Replace with actual column width
 		begin
 			col_count = 0;
 			row_count = row_count + 1;
-			if(row_count > 6) // TODO Replace with actual row size
+			if(row_count > `IMAGE_HEIGHT) // TODO Replace with actual row size
 			begin
 				row_count = 0;
 			end
 		end
-		if(strobe == 1) begin strobe = 0; end
-		else begin strobe = 1; end
-			
-	end 
+	end
 endmodule
--- YODA_IMA2/transfer_interface_tb.v
+++ YODA_IMA2/transfer_interface_tb.v
@@ -4,9 +4,9 @@
 // Company: 
 // Engineer:
 //
-// Create Date:   15:42:53 06/22/2017
+// Create Date:   18:56:44 06/22/2017
 // Design Name:   transfer_interface
-// Module Name:   C:/Users/rbcsj/Desktop/Yoda_git/YODA_Image_Masking_Accelerator/YODA_IMA2/transfer_interface_tb.v
+// Module Name:   /home/bluelabuser/Desktop/John/YODA_Image_Masking_Accelerator/YODA_IMA2/transfer_interface_tb.v
 // Project Name:  YODA_IMA2
 // Target Device:  
 // Tool versions:  
@@ -26,35 +26,79 @@ module transfer_interface_tb;
 
 	// Inputs
 	reg Clock;
-	reg [11:0] pixel_in;
 
 	// Outputs
 	wire [11:0] pixel_out;
 	wire [7:0] pix_row;
 	wire [8:0] pix_col;
-	wire strobe;
 
 	// Instantiate the Unit Under Test (UUT)
 	transfer_interface uut (
 		.Clock(Clock), 
-		.pixel_in(pixel_in), 
 		.pixel_out(pixel_out), 
 		.pix_row(pix_row), 
-		.pix_col(pix_col), 
-		.strobe(strobe)
+		.pix_col(pix_col)
 	);
 
 	initial begin
+		$monitor("clock = %d, pixel out = %d, row = %d, column = %d", Clock, pixel_out, pix_row, pix_col);
 		// Initialize Inputs
 		Clock = 0;
-		pixel_in = 0;
 
 		// Wait 100 ns for global reset to finish
 		#100;
-        
+            
 		// Add stimulus here
-
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#20 Clock = 1;
+		
+		#20 Clock = 0;
+		
+		#100 $finish;
 	end
       
 endmodule
-
