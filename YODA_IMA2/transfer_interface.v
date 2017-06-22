16a5bd4dc2525947859df14f15e47d2cbaee7d3f
John Odetokun <rbcs.john@gmail.com>
2017-06-22 15:00:18 +0200

    added to gitignore and created transfer interface

---
+++ YODA_IMA2/transfer_interface.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    14:58:51 06/22/2017 
+// Design Name: 
+// Module Name:    transfer_interface 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module transfer_interface(
+    );
+
+
+endmodule
