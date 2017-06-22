6463367fa7e0d0e4e7e4d4903d54c4cb9bb4eef4
John Odetokun <rbcs.john@gmail.com>
2017-06-22 15:03:03 +0200

    fixed image rom and added to gitignore

--- YODA_IMA2/transfer_interface.v
+++ YODA_IMA2/transfer_interface.v
@@ -1,25 +1,52 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    14:58:51 06/22/2017 
-// Design Name: 
-// Module Name:    transfer_interface 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-module transfer_interface(
-    );
+module transfer_interface(Clock, pixel_in, row_sel, col_sel, pixel_out, pix_row, pix_col, strobe);
+input Clock;
+input [11:0] pixel_in;
+
+output [11:0] pixel_out;
+output [8:0] pix_row;
+output [7:0] pix_col;
+output [8:0] row_sel;
+output [7:0] col_sel;
+output strobe;
 
 
+reg strobe;
+reg mode = 0; // mode = 0: row_sel and col_sel set for BRAM		mode = 1: infor written to IMM
+reg [11:0] pixel_out;
+reg [8:0] row_count = 0;
+reg [7:0] col_count = 0;
+reg [8:0] pix_row;
+reg [7:0] pix_col;
+reg [8:0] row_sel;
+reg [7:0] col_sel;
+
+	always @(posedge Clock) begin
+		if(mode == 0) begin
+			strobe = 0;
+			row_sel = row_count;
+			col_sel = col_count;
+			mode = 1;
+		end
+		else begin
+			pixel_out = pixel_in;
+			pix_row = row_count;
+			pix_col = col_count;
+			
+			col_count = col_count + 1;
+			if(col_count > 6) // TODO Replace with actual column width
+			begin
+				col_count = 0;
+				row_count = row_count + 1;
+				if(row_count > 6) // TODO Replace with actual row size
+				begin
+					row_count = 0;
+				end
+			end
+			
+			mode = 0;
+			strobe = 1;
+		end
+		
+		
+	end 
 endmodule
